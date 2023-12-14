89eb948056b361ec305147e2e3800a5e91f7d634
UnesseAh <youness.ahasla@gmail.com>
2023-12-14 04:48:13 +0100

    vaidation: add validation to LevelResponseVM

--- src/main/java/com/example/aftas/controller/vm/LevelRequestVM.java
+++ src/main/java/com/example/aftas/controller/vm/LevelRequestVM.java
@@ -1,11 +1,21 @@
 package com.example.aftas.controller.vm;
 
 import com.example.aftas.entities.Level;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 public record LevelRequestVM(
+        @NotNull(message = "Code can't be null")
+        @NotEmpty(message = "Code can't be empty")
         Integer code,
+        @NotBlank(message = "Description can't be blank")
+        @Size(min = 2, max = 255, message = "Description must be between 10 and 20 characters")
         String description,
+        @NotNull(message = "Code can't be null")
+        @NotEmpty(message = "Code can't be empty")
         Integer points
 ) {
     public Level toLevel(){
